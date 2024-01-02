7763786afe04aecc4bf14dcc5a7efa6262d5914b
Younes-Hounain <younesho9@outlook.com>
2024-01-02 21:38:53 +0100

    [CD] : Fixed & Validated the controller :wrench:

--- SpringCloud-Kafka-Streams-Demo-project/src/main/java/org/sid/springcloudkafkastreamsdemoproject/web/PageEventRestController.java
+++ SpringCloud-Kafka-Streams-Demo-project/src/main/java/org/sid/springcloudkafkastreamsdemoproject/web/PageEventRestController.java
@@ -1,6 +1,7 @@
 package org.sid.springcloudkafkastreamsdemoproject.web;
 
 import org.sid.springcloudkafkastreamsdemoproject.entities.PageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,11 @@ import java.util.Random;
 
 @RestController
 public class PageEventRestController {
+    @Autowired
     private StreamBridge streamBridge;
     @GetMapping("/publish/{topic}/{name}")
     public PageEvent publish(@PathVariable String topic,@PathVariable String name) {
-        PageEvent pageEvent = new PageEvent(name, "user_"+new Random().nextInt(100), new Date(), new Random().nextInt(10000));
+        PageEvent pageEvent = new PageEvent(name, Math.random()>0.5?"U1":"U2", new Date(), new Random().nextInt(9000));
         streamBridge.send(topic, pageEvent);
         return pageEvent;
     }
