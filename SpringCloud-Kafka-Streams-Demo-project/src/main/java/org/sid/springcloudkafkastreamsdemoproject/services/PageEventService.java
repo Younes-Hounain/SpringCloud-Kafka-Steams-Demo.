4cefc7be3e4fd853195548014a834ca676caec30
Younes-Hounain <younesho9@outlook.com>
2024-01-02 21:46:27 +0100

    [CD] : Added PageEventService & specified channel name on app.properties :new:

---
+++ SpringCloud-Kafka-Streams-Demo-project/src/main/java/org/sid/springcloudkafkastreamsdemoproject/services/PageEventService.java
@@ -0,0 +1,19 @@
+package org.sid.springcloudkafkastreamsdemoproject.services;
+
+import org.sid.springcloudkafkastreamsdemoproject.entities.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Service
+public class PageEventService {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return (input) -> {
+            System.out.println("******************");
+            System.out.println(input.toString());
+            System.out.println("******************");
+        };
+    }
+}
