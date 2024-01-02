873d8e414330596e760c0b6bfed19517e1aef0cf
Younes-Hounain <younesho9@outlook.com>
2024-01-02 22:30:54 +0100

    [CD] : Added new functionalities for diffirent channels :new:

--- SpringCloud-Kafka-Streams-Demo-project/src/main/java/org/sid/springcloudkafkastreamsdemoproject/services/PageEventService.java
+++ SpringCloud-Kafka-Streams-Demo-project/src/main/java/org/sid/springcloudkafkastreamsdemoproject/services/PageEventService.java
@@ -4,7 +4,11 @@ import org.sid.springcloudkafkastreamsdemoproject.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Service
 public class PageEventService {
@@ -16,4 +20,22 @@ public class PageEventService {
             System.out.println("******************");
         };
     }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return () -> new PageEvent(
+                Math.random()>0.5?"P1":"P2",
+                Math.random()>0.5?"U1":"U2",
+                new Date(),
+                new Random().nextInt(9000));
+    }
+
+    @Bean
+    public Function<PageEvent, PageEvent> pageEventFunction(){
+        return (input) -> {
+            input.setName(input.getName().toUpperCase());
+            input.setUser(input.getUser().toUpperCase());
+            return input;
+        };
+    }
 }
+
